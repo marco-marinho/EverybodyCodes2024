2dc6b6d0dab1d6229f0dd869924096b2a99a3aa8
Marco Marinho <marcoobom@gmail.com>
2025-12-16 18:37:29 +0100

    Quest 02

--- src/quests/quest02.rs
+++ src/quests/quest02.rs
@@ -1,9 +1,5 @@
 use crate::util;
 
-struct RuneState{
-    rune: Vec<u8>,
-    position: usize,
-}
 
 #[inline]
 fn count_runic_occurrences(runic: &[&str], word: &str) -> usize {
@@ -12,30 +8,6 @@ fn count_runic_occurrences(runic: &[&str], word: &str) -> usize {
          .count()
 }
 
-fn count_runic_in_phrase(runic: &mut [RuneState], phrase: &[&str]) -> usize {
-    let mut count = 0usize;
-    for curr in phrase.iter().flat_map(|&w| w.bytes()) {
-        for rune_state in runic.iter_mut() {
-            if rune_state.position < rune_state.rune.len() && curr == rune_state.rune[rune_state.position] {
-                rune_state.position += 1;
-                if rune_state.position == rune_state.rune.len() {
-                    count += rune_state.position;
-                    rune_state.position = 0;
-                }
-            }
-            else {
-                rune_state.position = 0;
-            }
-        }
-    }
-    count
-}
-
-fn reset_states(runic: &mut [RuneState]) {
-    for rune_state in runic.iter_mut() {
-        rune_state.position = 0;
-    }
-}
 
 fn get_runic(line: &str) -> Vec<&str> {
     line.split_once(':')
@@ -43,15 +15,76 @@ fn get_runic(line: &str) -> Vec<&str> {
         .unwrap_or_default()
 }
 
-fn get_runic_structs(line: &str) -> Vec<RuneState> {
+fn get_runic_as_bytes(line: &str) -> Vec<Vec<u8>> {
     let runes = get_runic(line);
     let forward= 
-            runes.iter().map(|x| RuneState{rune: x.as_bytes().to_vec(), position : 0usize});
+            runes.iter().map(|x| x.as_bytes().to_vec());
     let backward = 
-            runes.iter().map(|x| RuneState{rune: x.bytes().rev().collect(), position : 0usize});
+            runes.iter().map(|x| x.bytes().rev().collect());
     forward.chain(backward).collect()
 }
 
+fn count_runnic_chars(runic: &[Vec<u8>], phrase: &[u8]) -> usize {
+    let mut flags = vec![false; phrase.len()];
+    for i in 0..phrase.len(){
+        for rune in runic {
+            if phrase[i..].starts_with(rune) {
+                for j in 0..rune.len() {
+                    flags[i + j] = true;
+                }
+            }
+        }
+    }
+    flags.iter().filter(|&&f| f).count()
+}
+
+fn count_runnic_circular(runic: &[Vec<u8>], phrases: &[u8], rows: usize, cols: usize) -> usize {
+    let mut grid = vec![false; rows * cols];
+    for row in 0 .. rows{
+        for col in 0 .. cols{
+            for rune in runic {
+                for j in 0..rune.len() {
+                    let idx = (col + j) % cols;
+                    if phrases[row * cols + idx] != rune[j] {
+                        break;
+                    }
+                    if j == rune.len() - 1 {
+                        for k in 0..rune.len() {
+                            let flag_idx = (col + k) % cols;
+                            grid[row * cols + flag_idx] = true;
+                        }
+                    }
+                }
+                if rune.len() > rows - row {
+                    continue;
+                }
+                for j in 0..rune.len() {
+                    if phrases[(row + j) * cols + col] != rune[j] {
+                        break;
+                    }
+                    if j == rune.len() - 1 {
+                        for k in 0..rune.len() {
+                            grid[(row + k) * cols + col] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    grid.iter().filter(|&&f| f).count()
+}
+
+fn lines_to_grid(phrases: &Vec<Vec<u8>>) -> (Vec<u8>, usize, usize) {
+    let rows = phrases.len();
+    let cols = phrases[0].len();
+    let mut grid = vec![0u8; rows * cols];
+    for row in 0..rows {
+        for col in 0..cols {
+            grid[row * cols + col] = phrases[row][col];
+        }
+    }
+    (grid, rows, cols)
+}
 
 fn part1() -> String {
     let lines = &util::read_day(2, 1);
@@ -62,22 +95,23 @@ fn part1() -> String {
 }
 
 
-
 fn part2() -> String {
     let lines = &util::read_day(2, 2);
-    let mut runic= get_runic_structs(&lines[0]);
+    let runic= get_runic_as_bytes(&lines[0]);
 
-    let phrases = lines[2..].iter().map(|line| line.split(' ').collect::<Vec<&str>>());
-    let mut total = 0usize;
-    for phrase in phrases {
-        total += count_runic_in_phrase(&mut runic, &phrase);
-        reset_states(&mut runic);
-    }
-    total.to_string()
+    let total_count = lines.iter()
+                             .map(|line| count_runnic_chars(&runic, line.as_bytes()))
+                             .sum::<usize>();
+    total_count.to_string()
 }
 
 fn part3() -> String {
-    "Elefante".to_string()
+    let lines = &util::read_day(2,3);
+    let runic= get_runic_as_bytes(&lines[0]);
+    let phrases: Vec<Vec<u8>> = lines[2..].iter().map(|line| line.bytes().collect()).collect();
+    let (phrases_grid, rows, cols) = lines_to_grid(&phrases);
+    let count_rows = count_runnic_circular(&runic, &phrases_grid, rows, cols);
+    count_rows.to_string()
 }
 
 pub fn solve(part: u32) -> String {
