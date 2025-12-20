4a0d5cc0ac48aedbe65b9ac2002b8e1b62d48882
Marco Marinho <marcoobom@gmail.com>
2025-12-20 20:08:10 +0100

    Quest 08

--- Sources/everybody_codes_2024/quest08.swift
+++ Sources/everybody_codes_2024/quest08.swift
@@ -2,17 +2,23 @@ import Foundation
 
 enum Quest08 {
 
+    @inline(__always)
     private static func getThickness(input: Int, ratio: Int, modulo: Int)
         -> Int
     {
         return (input * ratio) % modulo
     }
 
+    @inline(__always)
+    private static func layerSize(layer: Int) -> Int {
+        return 2 * (layer - 1) + 1
+    }
+
     private static func part1() {
         let input = 4_097_633
         let closestSquare = Int(sqrt(Double(input)).rounded(.up))
         let totalCells = closestSquare * closestSquare
-        let baseSize = 2 * (closestSquare - 1) + 1
+        let baseSize = layerSize(layer: closestSquare)
         print((totalCells - input) * baseSize)
     }
 
@@ -21,13 +27,13 @@ enum Quest08 {
         var currentThickness = 1
         var layer = 1
         while available > 0 {
+            layer += 1
             currentThickness = getThickness(
                 input: currentThickness,
                 ratio: 924,
                 modulo: 1111
             )
-            layer += 1
-            available -= ((2 * (layer - 1) + 1) * currentThickness)
+            available -= layerSize(layer: layer) * currentThickness
         }
         print(abs(available) * (2 * (layer - 1) + 1))
     }
